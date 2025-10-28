dfa9454c3924b987b785ac47a3d46918ad1d99d4
Truella <turahsuleiman1212@gmail.com>
2025-10-28 13:39:58 +0100

    ...

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/Resolvr_React_Version/',
 	plugins: [react(), tailwindcss()],
 });
